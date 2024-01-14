942a2a7dc32831e7b0095ba3ade154f4af570a0c
Arif Hussain <arifh9821@gmail.com>
2024-01-15 01:40:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
-	let str ='';
-	let idx=0;
-	while (idx < s.length && s[idx] !==' ') {
-		str+=s[idx];
-		idx++;
-	}
-	return str;
+	if (str.trim() === '' || !str.includes(' ')) {
+    return str;
+  }
+
+  let result = '';
+  let index = 0;
+
+  // Iterate through characters until a space is encountered
+  while (index < str.length && str[index] !== ' ') {
+    result += str[index];
+    index++;
+  }
+
+  return result;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const str = prompt("Enter String:");
 alert(firstWord(s));
